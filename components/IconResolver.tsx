fc6d6aca30347ad680676d501b48645def427cde
Robin Edman <robin.edman@gmail.com>
2025-10-31 20:20:08 +0000

    font

--- components/IconResolver.tsx
+++ components/IconResolver.tsx
@@ -22,3 +22,7 @@ export const resolveIcon = (iconName: string, size = 14) => {
   const Icon = iconMap[iconName as keyof typeof iconMap];
   return Icon ? <Icon size={size} /> : null;
 };
+
+export const getIconComponent = (iconName: string) => {
+  return iconMap[iconName as keyof typeof iconMap];
+};
